5f955543db5bfd2e5dbb2e46da183efe565f1ff1
takashi365 <yuda365@gmail.com>
2019-10-06 21:46:12 +0900

    イベント発火、as json確認

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formdata = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formdata,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
+})
