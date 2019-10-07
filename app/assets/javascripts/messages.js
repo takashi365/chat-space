626bdb1057655436d5aa5e3bbf34341740f47e46
takashi365 <yuda365@gmail.com>
2019-10-07 13:04:31 +0900

    add

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,26 @@
 $(function(){
+  function buildHTML(message){
+    console.log(message);
+    var content = message.content ? '${message.content}' : "";
+    var img = message.image ? '<img src= ${message.image}>' : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__upper-info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message__text">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+  return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formdata = new FormData(this);
@@ -11,5 +33,24 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      $('#new_message')[0].reset();
+    })
+    .done(function scrollBottom(){
+      var target = $('.message').last();
+      var position = target.offset().top + $('.messages').scrollTop();
+      $('.messages').animate({
+        scrollTop: position
+      }, 300, "swing");
+    })
+    .fail(function(){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);
+    })
   })
-})
+});
