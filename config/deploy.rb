9a563d11182656471ec530f7b948e438c06d5965
takashi365 <yuda365@gmail.com>
2019-10-13 15:28:39 +0900

    updata

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:takashi365/chat-space.git"
+set :repo_url, "git@github.com:takashi365/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
