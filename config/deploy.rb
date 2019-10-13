a7f4d7e3691d873fa2674797ce8339f00074fe17
takashi365 <yuda365@gmail.com>
2019-10-13 17:21:47 +0900

    s3_add

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,10 +4,16 @@ lock "~> 3.11.2"
 set :application, "chat-space"
 set :repo_url, "git@github.com:takashi365/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
+set :linked_files, %w{ config/secrets.yml }
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/shikatamajikana.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
@@ -20,6 +26,18 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
 
 # Default branch is :master
